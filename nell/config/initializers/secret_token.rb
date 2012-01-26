60c1dad3dd50c2f9dad5c7892c0d474004cd5776
Liuyi Sun <sly@cmu-786363.wv.cc.cmu.edu>
2012-01-25 21:17:15 -0500

    initialize

---
+++ nell/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nell::Application.config.secret_token = '05a3255d40f16508cfcb8166d3f5ca82108145a359b9e32e2216b9fb7d33970e8186f8f9a0deddb64ff4b74e507c85b84c8401c2c1801041a5afd30aa07bd97e'
